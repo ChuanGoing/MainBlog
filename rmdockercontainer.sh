f8eead2b6fc2c97b6ce097c0a7714de26e928021
Caocb <caochuanbei@51testing.com>
2020-05-26 15:24:04 +0800

    测试

--- rmdockercontainer.sh
+++ rmdockercontainer.sh
@@ -1,10 +1,11 @@
 vname=$1
-name=$(docker ps|grep $vname|awk -F '[ ]{3,}' 'NR==1 {print $7}')
+#name=$(docker ps|grep $vname|awk -F '[ ]{3,}' 'NR==1 {print $7}')
+id=$(docker ps -a|grep $vname|awk 'NR==1 {print $1}')
 
-if [ "$name" != "" ];
+if [ "$id" != "" ];
 then
-    echo "docker stop $name ..."
-    docker stop $name
+    echo "docker stop $id ..."
+    docker stop $id
 fi
 
 id=$(docker ps -a|grep $vname|awk 'NR==1 {print $1}')
