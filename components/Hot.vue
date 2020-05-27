48e9bd3c4e512f370873fa45a5f9c5dbfef751a9
Caocb <caochuanbei@51testing.com>
2020-05-27 15:42:18 +0800

    简化

--- components/Hot.vue
+++ components/Hot.vue
@@ -1,211 +1,88 @@
 <template>
   <div class="new">
-    <div class="img">
-      <img
-        src="/img/icons/hot.png"
-        alt
-      />
-    </div>
-
-    <template>
-      <el-tabs
-        v-model="activeName"
-        @tab-click="handleClick"
+    <el-tabs
+      v-model="activeName"
+      @tab-click="handleClick"      
+      tab-position="top"
+    >
+      <el-tab-pane
+        label="最新"
+        name="first"
       >
-        <el-tab-pane
-          label="最新"
-          name="first"
-        >
-          <div class="bot clearfix">
+        <div class="bot clearfix">
+          <div
+            v-for="(item, index) in adNewList"
+            v-show="index < 4"
+            :key="index"
+            class="api"
+          >
             <div
-              v-for="(item, index) in adNewList"
-              v-show="index < 4"
-              :key="index"
-              class="api"
+              :style="{ 'background-image': `url('${item.img}')` }"
+              class="img1"
             >
               <div
-                :style="{ 'background-image': `url('${item.img}')` }"
-                class="img1"
+                v-if="item.tags"
+                class="skill"
               >
-                <div
-                  v-if="item.tags"
-                  class="skill"
-                >
-                  <template v-for="(el, i) in transArr(item.tags)">
-                    <div
-                      v-if="i<=1"
-                      :key="i"
-                      :title="el"
-                      class="ellipsis"
-                    >{{ el }}</div>
-                  </template>
-                  <!-- <div :title="item.CategoryName" class="ellipsis">{{item.CategoryName}}</div> -->
-                </div>
-                <span
-                  v-if="item.mode == 2"
-                  class="live"
-                >直播</span>
-                <div
-                  v-if="item.mode == 2"
-                  class="python"
-                >
-                  <template v-if="item.isLive">
-                    <span>正在直播</span>
-                    <img
-                      src="/img/zyx.gif"
-                      alt
-                    />
-                  </template>
-                  <span v-if="item.liveStartTime > 0 && !item.isLive">
-                    {{
-                    $moment(item.liveStartTime * 1000).format('MM月DD日 HH:mm')
-                    }}
-                  </span>
-                </div>
+                <template v-for="(el, i) in transArr(item.tags)">
+                  <div
+                    v-if="i<=1"
+                    :key="i"
+                    :title="el"
+                    class="ellipsis"
+                  >{{ el }}</div>
+                </template>
+                <!-- <div :title="item.CategoryName" class="ellipsis">{{item.CategoryName}}</div> -->
               </div>
-              <div>
-                <div
-                  :title="item.title"
-                  class="auto ellipsis"
-                >{{ item.title }}</div>
-                <div class="price">
-                  <template>
-                    <span
-                      v-if="item.chargeMode === 1"
-                      class="original"
-                    >
-                      <b>免费</b>
-                    </span>
-                    <span
-                      v-else-if="item.chargeMode === 3"
-                      class="original"
-                    >
-                      <b>面议</b>
-                    </span>
-                    <span
-                      v-else-if="item.chargeMode === 2"
-                      class="original"
-                    >
-                      <b v-if="item.promotionPrice">￥{{ item.promotionPrice }}</b>
-                      <b v-else>￥{{ item.price }}</b>
-                    </span>
-                  </template>
-                  <span
-                    v-if="item.promotionPrice"
-                    class="saleprice"
-                  >￥{{ item.price }}</span>
-                  <span class="people right">{{ item.sales }}</span>
-                  <img
-                    class="right"
-                    src="../static/img/sales.png"
-                    alt
-                  />
-                  <span class="right diffcult-rank">{{fillter_diffcultLeval(item.difficultylevel)}}</span>
-                </div>
-              </div>
-              <!-- <span style="color:#0898ef;white-space: nowrap;">{{item.Duration}}</span> -->
             </div>
-            <!-- </nuxt-link> -->
-          </div>
-        </el-tab-pane>
-        <el-tab-pane
-          label="最热"
-          name="second"
-        >
-          <div class="bot clearfix">
-            <div
-              v-for="(item, index) in adHotList"
-              v-show="index < 4"
-              :key="index"
-              class="api"
-            >
+            <div>
               <div
-                :style="{ 'background-image': `url('${item.img}')` }"
-                class="img1"
-              >
-                <!-- <img :src="item.Icon" alt> -->
-                <div class="skill">
-                  <template v-for="(el, i) in transArr(item.tags)">
-                    <div
-                      v-if="i<=1"
-                      :key="i"
-                      :title="el"
-                      class="ellipsis"
-                    >{{ el }}</div>
-                  </template>
-                </div>
-                <span
-                  v-if="item.mode == 2"
-                  class="live"
-                >直播</span>
-                <div
-                  v-if="item.mode == 2"
-                  class="python"
-                >
-                  <template v-if="item.isLive">
-                    <span>正在直播</span>
-                    <img
-                      src="/img/zyx.gif"
-                      alt
-                    />
-                  </template>
-                  <span v-if="item.liveStartTime > 0 && !item.isLive">
-                    {{
-                    $moment(item.liveStartTime * 1000).format('MM月DD日 HH:mm')
-                    }}
-                  </span>
-                </div>
-                <!-- <div v-if="item.Mode==2 && item.Duration" class="python">
-                  <img v-if="item.Duration=='正在直播'" src="/img/zyx.gif" alt />
-                  <span>直播时间</span>
-                  {{$moment(item.duration * 1000).format('YYYY.MM.DD')}}~{{$moment(item.expiryEndDate * 1000).format('YYYY.MM.DD')}}
-                </div>-->
-              </div>
-              <div>
-                <div
-                  :title="item.title"
-                  class="auto ellipsis"
-                >{{ item.title }}</div>
-                <div class="price">
-                  <template>
-                    <span
-                      v-if="item.chargeMode === 1"
-                      class="original"
-                    >
-                      <b>免费</b>
-                    </span>
-                    <span
-                      v-else-if="item.chargeMode === 3"
-                      class="original"
-                    >
-                      <b>面议</b>
-                    </span>
-                    <span
-                      v-else-if="item.chargeMode === 2"
-                      class="original"
-                    >
-                      <b v-if="item.promotionPrice">￥{{ item.promotionPrice }}</b>
-                      <b v-else>￥{{ item.price }}</b>
-                    </span>
-                  </template>
-                  <span
-                    v-if="item.promotionPrice"
-                    class="saleprice"
-                  >￥{{ item.price }}</span>
-                  <span class="people right">{{ item.sales }}</span>
-                  <img
-                    class="right"
-                    src="../static/img/sales.png"
-                    alt
-                  />
-                  <span class="right diffcult-rank">{{fillter_diffcultLeval(item.difficultylevel)}}</span>
-                </div>
+                :title="item.title"
+                class="auto ellipsis"
+              >{{ item.title }}</div>
+            </div>
+            <!-- <span style="color:#0898ef;white-space: nowrap;">{{item.Duration}}</span> -->
+          </div>
+          <!-- </nuxt-link> -->
+        </div>
+      </el-tab-pane>
+      <el-tab-pane
+        label="更多"
+        name="second"
+      >
+        <div class="bot clearfix">
+          <div
+            v-for="(item, index) in adHotList"
+            v-show="index < 4"
+            :key="index"
+            class="api"
+          >
+            <div
+              :style="{ 'background-image': `url('${item.img}')` }"
+              class="img1"
+            >
+              <!-- <img :src="item.Icon" alt> -->
+              <div class="skill">
+                <template v-for="(el, i) in transArr(item.tags)">
+                  <div
+                    v-if="i<=1"
+                    :key="i"
+                    :title="el"
+                    class="ellipsis"
+                  >{{ el }}</div>
+                </template>
               </div>
             </div>
+            <div>
+              <div
+                :title="item.title"
+                class="auto ellipsis"
+              >{{ item.title }}</div>
+            </div>
           </div>
-        </el-tab-pane>
-      </el-tabs>
-    </template>
+        </div>
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
