4ab8ea7f91b27e1301f082ebaccd55d99c99db9d
Caocb <caochuanbei@51testing.com>
2021-01-22 16:32:31 +0800

    备案号

--- components/Foot.vue
+++ components/Foot.vue
@@ -101,7 +101,7 @@
               src="/img/record-icon.png"
               alt=""
             />
-            <p>沪ICP备2021001447</p>
+            <p>沪ICP备2021001447号</p>
           </a>
         </p>
         <p>
@@ -111,7 +111,7 @@
             target="_blank"
             class="footer-cert"
           >
-            沪ICP备2021001447-1
+            沪ICP备2021001447号-1
           </a>
         </p>
       </div>
